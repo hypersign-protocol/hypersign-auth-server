81942dc235dd0e029fe1290e0a586a9ee9fcadd5
vishwas1 <vishwasanand.cse@gmail.com>
2020-10-24 19:00:08 +0530

    superhero template

--- src/mailTemplates/registration.ts
+++ src/mailTemplates/registration.ts
@@ -14,14 +14,14 @@ const template =
   <body>
     <div id='body'>
       <p>Hi @@RECEIVERNAME@@,</p>
-      <p>Welcome to Hypersign!</p>
+      <p>Welcome to Superhero!</p>
       <p></p>
-      <p class='colored'>Please click on this link or scan the QR code (attached with this email) to get the Hypersign Auth Credential to be able to login into websites that supports Hypersign login. This link will expired in 15 minutes.</p>
-      <p><a href="@@LINK@@" target="_blank">Link to download HypersignAuth Credential</a></p>
+      <p class='colored'>Please click on this link or scan the QR code to get the Superhero Auth Credential to be able to login into websites that supports Superhero login. This link will expired in 15 minutes.</p>
+      <p><a href="@@LINK@@" target="_blank">Link to download SuperheroAuth Credential</a></p>
       <p></p>
       <p><img alt="Logo" title="Logo" style="display:block" src="@@QRURL@@"></p>
       <p>Thanks & Regards,</p>
-      <p>Team Hypersign!</p>
+      <p>Team Superhero!</p>
     </div>
   </body>
 </html>
