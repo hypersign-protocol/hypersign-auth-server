699e7f09c6d0fe1a84d20e66432754471f29978e
vishwas1 <vishwasanand.cse@gmail.com>
2021-04-17 14:30:23 +0530

    updated

--- src/app.ts
+++ src/app.ts
@@ -30,6 +30,7 @@ app.use(xss());
 app.use(cors());
 app.use(express.json({ limit: '10kb' }));
 
+app.get('/', (req, res) =>  res.send("Hello from auth server"))
 app.use('/hs/api/v2/', routes.auth(hypersign));
 
 app.listen(PORT, () => console.log('Server is running @ ' + baseUrl));
