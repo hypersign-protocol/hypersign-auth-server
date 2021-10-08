3f49d8fac760306449feb289306c2573c2c65278
Pratap Mridha <pratapmridha@gmail.com>
2021-10-08 20:36:45 +0530

    minor bug fixed

--- src/middleware/registerSchema.ts
+++ src/middleware/registerSchema.ts
@@ -3,7 +3,7 @@ import { body, param, query } from 'express-validator';
 
 export const registerSchemaBody=[
     body('name').exists({ checkFalsy: true }).trim().withMessage('Name can not be null or empty'),
-    body('name').isAlpha().trim().withMessage('Name Cannot be anything other than text'),
+    body('name').isAlpha('en-US',{ignore:' '}).trim().withMessage('Name Cannot be anything other than text'),
     body('email').exists({ checkFalsy: true }).trim().withMessage('email can not be null or empty'),
     body('email').isEmail().trim().withMessage('Email is not valid'),
 ]
