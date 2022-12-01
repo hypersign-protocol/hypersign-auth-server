90667b399514d56f9975864e4edd04ae5580b6d7
Pratap Mridha <pratapmridha@gmail.com>
2022-12-01 18:00:22 +0530

    url sort

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express  from 'express';
+import express from 'express';
 import authRoutes from './routes/auth';
 import walletRoutes from './routes/wallet';
 import { PORT, baseUrl, whitelistedUrls, HIDNODE_RPC_URL, HIDNODE_REST_URL, HID_WALLET_MNEMONIC } from './config';
@@ -7,7 +7,8 @@ import cors from 'cors';
 import HypersignAuth from 'hypersign-auth-node-sdk';
 import http from 'http';
 import HIDWallet from 'hid-hd-wallet';
-
+import hsSSIdk from 'hs-ssi-sdk'
+import vpschema from './models/vp';
 
 const app = express();
 
@@ -20,9 +21,9 @@ const limiter= rateLimit({
   });
 */
 
-const server =  http.createServer(app);
+const server = http.createServer(app);
 
-function corsOptionsDelegate (req, callback) {
+function corsOptionsDelegate(req, callback) {
   let corsOptions;
   if (whitelistedUrls.indexOf(req.header('Origin')) !== -1) {
     corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
@@ -34,12 +35,12 @@ function corsOptionsDelegate (req, callback) {
 
 
 app.use(xss());
-app.use(cors(corsOptionsDelegate)); 
+app.use(cors(corsOptionsDelegate));
 app.use(express.json({ limit: '10kb' }));
 app.use(express.static('public'))
 
 // TODO:  this should go into hypersisgn auth sdk
-interface IHypersignAuth{
+interface IHypersignAuth {
 
   init(): Promise<void>;
 
@@ -63,16 +64,79 @@ const walletOptions = {
 };
 
 const hidWalletInstance = new HIDWallet(walletOptions);
-hidWalletInstance.generateWallet({mnemonic: HID_WALLET_MNEMONIC}).then(async() => {
+hidWalletInstance.generateWallet({ mnemonic: HID_WALLET_MNEMONIC }).then(async () => {
   hidWalletInstance.offlineSigner.getAccounts().then(console.log)
+
   const hypersign: IHypersignAuth = (new HypersignAuth(server, hidWalletInstance.offlineSigner)) as IHypersignAuth
+  const hsSSIdkInstance = new hsSSIdk(hidWalletInstance.offlineSigner, HIDNODE_RPC_URL, HIDNODE_REST_URL, 'testnet')
+  await hsSSIdkInstance.init();
   await hypersign.init();
 
   app.use('/hs/api/v2', authRoutes(hypersign));
   app.use('/hs/api/v2', walletRoutes(hidWalletInstance));
+  app.get('/shared/vp/:id', async (req, res) => {
+    try {
+
+      const id = req.params.id;
+      const vp = await vpschema.findOne({ _id: id }).exec();
+
+      res.status(200).json(vp);
+
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+
+
+  })
+  app.post('/share', async (req, res) => {
+    try {
+      const { vp } = req.body;
+      const issuerDid = vp.verifiableCredential[0].issuer
+      const holderDid = vp.verifiableCredential[0].credentialSubject.id
+      // console.log(holderDid);
+      const challenge = vp.proof.challenge
+      // console.log(challenge);
+      const issuerVerificationMethodId = vp.verifiableCredential[0].proof.verificationMethod
+      const holderVerificationMethodId = vp.proof.verificationMethod
+      const result = await hsSSIdkInstance.vp.verifyPresentation({
+        signedPresentation: vp,
+        challenge,
+        issuerDid,
+        holderDid,
+        holderVerificationMethodId,
+        issuerVerificationMethodId
+
+      });
+      if (result.verified) {
+
+        const record = await vpschema.create({ vp });
+
+        res.status(200).json({
+          status: 'success',
+          
+            record,
+            verified: result.verified,
+          
+
+        })
+      } else {
+        throw new Error('vp not verified')
+      }
+    } catch (error) {
+      res.status(500).json({
+        status: 'error',
+        message: error.message
+      })
+    }
+
+
+
+  })
+
+
 })
-.catch(e => {
-        console.error(e)
-    })
+  .catch(e => {
+    console.error(e)
+  })
 
 app.listen(PORT, () => console.log('Server is running @ ' + baseUrl));
